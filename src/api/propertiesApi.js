30a64e1b3202d42a4ddd0953b9f9c4587b46a4bc
Elisabeta Nitura <hello@elisabeta.me>
2021-02-06 20:17:00 +0100

    Fizing proxying

--- src/api/propertiesApi.js
+++ src/api/propertiesApi.js
@@ -3,8 +3,8 @@ import httpClient from "./httpClient";
 const apiKey = process.env.VUE_APP_FUNDA_API_KEY;
 
 const endpoints = {
-  property: id => `/api/detail/${apiKey}/koop/${id}/`,
-  search: `/api/${apiKey}/`
+  property: id => `api/detail/${apiKey}/koop/${id}/`,
+  search: `api/${apiKey}/`
 };
 
 const getProperty = id => httpClient.get(endpoints.property(id));
